4aa736cb283fb176ff02426453a380723d59538c
joker30oko <ronaldperez9499107@gmail.com>
2024-11-08 00:55:07 +0100

    Add files via upload

---
+++ modules/brevo.py
@@ -0,0 +1,27 @@
+def get_remaining_sends(api_key):
+    url = "https://api.brevo.com/v3/account"
+    
+    headers = {
+        "accept": "application/json",
+        "api-key": api_key  # Используем переданный API-ключ
+    }
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        account_info = response.json()
+        print("Account Information:", account_info)
+        
+        # Получите количество оставшихся отправок
+        remaining_sends = None
+        for plan in account_info.get('plan', []):
+            if plan['creditsType'] == 'sendLimit':
+                remaining_sends = plan['credits']
+                break
+        
+        if remaining_sends is not None:
+            return remaining_sends
+        else:
+            return "Не удалось найти оставшиеся отправки."
+    else:
+        return f"Ошибка: {response.status_code}, {response.text}"
