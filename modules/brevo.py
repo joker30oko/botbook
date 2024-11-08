2e7c33fa18ad7c976cb7c0d094b75f3538d5ad93
joker30oko <ronaldperez9499107@gmail.com>
2024-11-08 13:58:38 +0100

    Add files via upload

--- modules/brevo.py
+++ modules/brevo.py
@@ -1,6 +1,6 @@
-import requests
+import aiohttp
 
-def get_remaining_sends(api_key):
+async def get_remaining_sends(api_key):
     url = "https://api.brevo.com/v3/account"
     
     headers = {
@@ -8,21 +8,21 @@ def get_remaining_sends(api_key):
         "api-key": api_key  # Используем переданный API-ключ
     }
     
-    response = requests.get(url, headers=headers)
-    
-    if response.status_code == 200:
-        account_info = response.json()
-        
-        # Получите количество оставшихся отправок
-        remaining_sends = None
-        for plan in account_info.get('plan', []):
-            if plan['creditsType'] == 'sendLimit':
-                remaining_sends = plan['credits']
-                break
-        
-        if remaining_sends is not None:
-            return remaining_sends
-        else:
-            return "Не удалось найти оставшиеся отправки."
-    else:
-        return f"Ошибка: {response.status_code}, {response.text}"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers) as response:
+            if response.status == 200:
+                account_info = await response.json()
+                
+                # Получите количество оставшихся отправок
+                remaining_sends = None
+                for plan in account_info.get('plan', []):
+                    if plan['creditsType'] == 'sendLimit':
+                        remaining_sends = plan['credits']
+                        break
+                
+                if remaining_sends is not None:
+                    return remaining_sends
+                else:
+                    return "Не удалось найти оставшиеся отправки."
+            else:
+                return f"Ошибка: {response.status}, {await response.text()}"
