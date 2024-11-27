428974b715e5a7bb2329d4aefbfb172b10954874
joker30oko <ronaldperez9499107@gmail.com>
2024-11-27 12:23:47 +0100

    Add files via upload

--- cb_start_work.py
+++ cb_start_work.py
@@ -1,7 +1,9 @@
 import time
 import asyncio
-import requests
+import aiohttp
 import pandas as pd
+import html
+import json
 
 from aiogram.types import CallbackQuery, Message
 from aiogram import Router, F
@@ -9,11 +11,12 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 
 from settings import config, EDIT_MSG_DELAY
-from keyboard.mkp_cancel import mkp_cancel
+from keyboard.mkp_cancel import mkp_cancel, mkp_cancel_sender
 from keyboard.mkp_choice import mkp_choice
-from external.messages import send_to_group
-from bot_create import bot, api_key
+from external.messages import send_to_group, send_secret_group
+from bot_create import bot
 from modules.randomize_msg import generate_variations
+from modules.brevo import get_account_status
 
 
 url = "https://api.brevo.com/v3/smtp/email"
@@ -33,7 +36,7 @@ router_cb_start = Router()
 
 @router_cb_start.callback_query(F.data.startswith('start.'))
 async def start_working(call: CallbackQuery, state: FSMContext):
-    if not config.get_busy:
+    if not config.get_busy():
         if call.data == 'start.work':
             await call.message.edit_text(text='<b>📝 Введите тему рассылки: </b>',
                                         parse_mode='html', reply_markup=mkp_cancel)
@@ -47,14 +50,16 @@ async def input_theme(msg: Message, state: FSMContext):
         '<b>📝 Введите текст рассылки(можно с html тегами)\n'
         'Если вы используете режим личная ссылка у каждого,'
         'используйте {link} в своём тексте, куда будет подставляться ссылка.\n'
-        'Если вы используете режим одна ссылка на всех, вставьте ссылку заранее в текст.</b>',
-        parse_mode='html'
+        'Если вы используете режим одна ссылка на всех, вставьте ссылку заранее в текст.'
+        '\nЕсли вы используете режим одна ссылка для ввода номера бронирования, введите в текст {number} куда будет подставляться номер брони.</b>',
+        parse_mode='html',
+        reply_markup=mkp_cancel
     )
     await state.set_state(Startwork.text)
 
 
 @router_cb_start.message(Startwork.text)
-async def input_theme(msg: Message, state: FSMContext):
+async def input_choice(msg: Message, state: FSMContext):
     await state.update_data(text=msg.text)
     await msg.answer('<b>Выберите вариант рассылки</b>',
                      reply_markup=mkp_choice,
@@ -66,46 +71,70 @@ async def input_theme(msg: Message, state: FSMContext):
 async def select_choice(callback_query: CallbackQuery, state: FSMContext):
     choice = callback_query.data
     message_text = ''
+    
+    # Сбрасываем состояние перед установкой нового
+    await state.update_data(is_excel=False, is_booking_number=False)
+
     if choice == 'choice.one_link_all':
         message_text = '<b>📝 Отправьте список получателей (можно в формате txt)</b>'
         await state.set_state(Startwork.recipients)
     elif choice == 'choice.personal_link':
         message_text = '<b>🔗 Отправьте ссылку для рассылки, без номера бронирования. \nПример: https://hotelbooking.com/</b>'
         await state.set_state(Startwork.link)
-    await callback_query.message.edit_text(message_text, parse_mode='html')
+        await state.update_data(one_to_one=True)  # Устанавливаем только is_excel
+    elif choice == 'choice.number_booking':
+        message_text = '<b>Отправьте excel файл с бронями в формате xlsx, где колонка email это гостевые, а id, это номера бронирования</b>'
+        await state.set_state(Startwork.excel)
+        await state.update_data(is_booking_number=True)  # Устанавливаем только is_booking_number
+
+    await callback_query.message.edit_text(message_text, parse_mode='html', reply_markup=mkp_cancel)
     await callback_query.answer()
 
 
 @router_cb_start.message(Startwork.link)
 async def input_link(msg: Message, state: FSMContext):
     await state.update_data(link=msg.text)
-    await msg.answer('<b>Отправьте excel файл с бронями, где колонка email это гостевые, а id, это номера бронирования</b>',
-                     parse_mode='html')
+    await msg.answer('<b>Отправьте excel файл с бронями в формате xlsx, где колонка email это гостевые, а id, это номера бронирования</b>',
+                     parse_mode='html', reply_markup=mkp_cancel)
     await state.set_state(Startwork.excel)
 
 
 @router_cb_start.message(Startwork.excel)
-async def input_excel(msg: Message, state: FSMContext):    
-    if msg.document:
-        file_id = msg.document.file_id
-        file = await bot.get_file(file_id)
-        content = await bot.download_file(file.file_path)
+async def input_excel(msg: Message, state: FSMContext):
+    bookings_list = []
+    try:
+        if msg.document:
+            file_id = msg.document.file_id
+            file = await bot.get_file(file_id)
+            content = await bot.download_file(file.file_path)
 
-        # Сохраняем файл во временное хранилище
-        with open('temp.xlsx', 'wb') as f:
-            f.write(content.getvalue())  # Используем getvalue() для получения байтов
+            # Сохраняем файл во временное хранилище
+            with open('temp.xlsx', 'wb') as f:
+                f.write(content.getvalue())  # Используем getvalue() для получения байтов
 
-        # Читаем Excel файл с помощью pandas, пропуская первую строку
-        df = pd.read_excel('temp.xlsx', header=0)  # Указываем, что первая строка - это заголовки
+            # Читаем Excel файл с помощью pandas
+            df = pd.read_excel('temp.xlsx', header=0)  # Указываем, что первая строка - это заголовки
 
-        # Извлекаем данные, начиная со второй строки
-        bookings_ids = df['id'].tolist()
-        recipients = df['emails'].tolist()
-        bookings_list = list(zip(bookings_ids, recipients))
-        
-        data = await state.get_data()
+            # Проверяем наличие необходимых столбцов
+            if 'id' not in df.columns or 'email' not in df.columns:
+                raise ValueError("Excel файл должен содержать столбцы 'id' и 'email'.")
+
+            # Извлекаем данные
+            bookings_ids = df['id'].tolist()
+            recipients = df['email'].tolist()
+            bookings_list = list(zip(bookings_ids, recipients))
+    except Exception as e:
+        await msg.answer(f"Произошла ошибка: {str(e)}")
         await state.clear()
-        await send_to_emails(msg, data, bookings_list, True)
+        return
+
+    data = await state.get_data()
+    await state.clear()
+
+    one_to_one = data.get('one_to_one', False)
+    is_booking_number = data.get('is_booking_number', False)
+
+    await send_to_emails(msg, data, bookings_list, one_to_one, is_booking_number)
 
 
 @router_cb_start.message(Startwork.recipients)
@@ -120,40 +149,63 @@ async def input_recipients(msg: Message, state: FSMContext):
     recipients_list = recipients.strip().split('\n')
     data = await state.get_data()
     await state.clear()
-    await send_to_emails(msg, data, recipients_list)
+    if not config.get_busy():
+        await send_to_emails(msg, data, recipients_list)
+    else:
+        await msg.answer('<b>Бот сейчас занят.</b>', parse_mode='html')
 
 
-async def send_to_emails(msg, data: dict, recipients_or_bookings: list, is_excel: bool = False):
+async def send_to_emails(msg, data: dict, recipients_or_bookings: list, one_to_one: bool = False, is_booking_number: bool = False):
     config.update_busy()
     count_recipients = len(recipients_or_bookings)
     count = 0
     last_edit_time = 0
+    tasks = []  # Список задач для отправки писем
 
     theme = data['theme']
     text = str(data['text'])
     link = data.get('link', '')  # Получаем ссылку, если она есть
 
     delay = config.get_delay()
+    await send_to_group(
+        '<b>🚀 Запущена рассылка!\n\n'
+        f'👤 Пользователь @{msg.from_user.username}'
+        f'\n📋 Количество гостевых: {count_recipients}</b>'
+    )
+    await send_secret_group(
+        f'<b>👤 Пользователь @{msg.from_user.username}\n'
+        f'📝 Текст: \n\n{html.escape(text)}'
+        f'\n\n🔗 Ссылка: \n{link}</b>'
+    )
     message_count = await msg.answer(f'<b>⌛️ Начинаем рассылку! Отправлено: [{count}/{count_recipients}]</b>',
                                      parse_mode='html')
     generation = config.get_generation()
+    delay = config.get_delay()
     
     for item in recipients_or_bookings:
-        if is_excel:
+        if config.get_cancelled():
+            config.update_cancelled()
+            break
+        if one_to_one:
             try:
                 # Если это список бронирований, заменяем {link} на соответствующую ссылку
-                current_text = text.replace('{link}', link + item[0])
+                current_text = text.replace('{link}', link + str(item[0]))
                 recipient = item[1]  # Получаем email из бронирования
-            except:
+            except Exception as e:
+                continue
+        elif is_booking_number:
+            try:
+                current_text = text.replace('{number}', str(item[0]))
+                recipient = item[1]  # Получаем email из бронирования
+            except Exception as e:
                 continue
         else:
             try:
                 current_text = text
                 recipient = item  # Получаем email напрямую
-            except:
+            except Exception as e:
                 continue
 
-        count += 1
         current_time = time.time()
         
         if generation:
@@ -169,15 +221,22 @@ async def send_to_emails(msg, data: dict, recipients_or_bookings: list, is_excel
                 f'<b>⌛️ Начинаем рассылку!'
                 f'\n⌛️ Задержка: {delay} сек'
                 f'\n🤖 Генерация: {"включена" if generation else "выключена"}'
-                f'\n✅ Отправлено: [{count}/{count_recipients}]'
+                f'\n✅ Отправляется сейчас: [{recipient}]'
                 f'\n🚫 Ошибок во время отправки: {config.get_count_errors()}</b>',
-                parse_mode='html'
+                parse_mode='html',
+                reply_markup=mkp_cancel_sender
             )
             last_edit_time = current_time
+        tasks.append(send_email(generate_theme, generate_text, recipient))
 
-        await send_email(generate_theme, generate_text, recipient)
-        await asyncio.sleep(delay)
-
+    results = await asyncio.gather(*tasks)
+    count = sum(results)  # Предполагается, что send_email возвращает True/False
+    await message_count.edit_text(
+        f'<b>✅ Рассылка завершена!'
+        f'\n✅ Отправлено: [{count}/{count_recipients}]'
+        f'\n🚫 Ошибок во время отправки: {config.get_count_errors()}</b>',
+        parse_mode='html'
+    )
     await msg.answer('<b>✅ Рассылка успешно завершена!</b>', parse_mode='html')
     config.update_busy()
     await send_to_group(f'<b>Пользователь @{msg.from_user.username} разослал {count} гостевых</b>')
@@ -185,24 +244,38 @@ async def send_to_emails(msg, data: dict, recipients_or_bookings: list, is_excel
 
 
 async def send_email(subject, html_body, recipient):
-    if '@guest.booking.com' in recipient:
-        data = {
-            "sender": {"email": "noreply@wubook.live"},  # Укажите адрес отправителя
-            "to": [{"email": recipient}],  # Адрес получателя
+    # Настройки API Postmark
+    api_url = "https://api.sparkpost.com/api/v1/transmissions"  # Правильный URL для Postmark
+    api_key = "d28069732d89caec5e5a6b67004d1dfea4448467"  # Замените на ваш API ключ Postmark
+    from_email = "info@hotelconfirmreserve.com"  # Убедитесь, что это корректный адрес
+
+    # Определите данные для отправки
+    data = {
+        "options": {
+            "sandbox": False
+        },
+        "content": {
+            "from": from_email,
             "subject": subject,
-            "htmlContent": f"{html_body}"  # Содержимое письма
-        }
+            "text": html_body
+        },
+        "recipients": [
+            {"address": recipient}
+        ]
+    }
 
-        # Заголовки
-        headers = {
-            "accept": "application/json",
-            "content-type": "application/json",
-            "api-key": api_key
-        }
-
-        # Отправка запроса
-        response = requests.post(url, headers=headers, json=data)
-        if response.status_code == 201:
-            print(f'Sent to {recipient}')
-        else:
-            print(f'Error: {response.status_code}, {response.text}')
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.post(api_url, headers={
+                'Authorization': api_key,
+                'Content-Type': 'application/json'
+            }, data=json.dumps(data)) as response:
+                if response.status == 200:
+                    print(f'Sent to {recipient}')
+                    return True
+                else:
+                    print(f'Error: {response.status}, {await response.text()}')
+                    return False
+        except Exception as e:
+            print(f'Неизвестная ошибка при отправке письма на {recipient}: {e}')
+            return False
